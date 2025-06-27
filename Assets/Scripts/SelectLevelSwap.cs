0f0b765b5379b382d62f42c05330aba6e4f2a87c
Murttraz <gpowers6@students.kennesaw.edu>
2025-06-27 10:17:45 -0400

    Menu Changes

--- Assets/Scripts/SelectLevelSwap.cs
+++ Assets/Scripts/SelectLevelSwap.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class SelectLevelSwap : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+public class SelectLevelSwap : MonoBehaviour {
+
+    public Button tutorialButton;
+    public Button map1Button;
+    public Button map2Button;
+    public Button map3Button;
+    public Button menuButton;
+
+    void Start() {
         
+        menuButton.onClick.AddListener(SwapTutorialLevel);
+        map1Button.onClick.AddListener(SwapLevel1);
+        map2Button.onClick.AddListener(SwapLevel2);
+        map3Button.onClick.AddListener(SwapLevel3);
+        menuButton.onClick.AddListener(SwapMenu);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SwapTutorialLevel() {
+        SceneManager.LoadScene("TutorialLevel");
     }
+
+    public void SwapLevel1() {
+        SceneManager.LoadScene("Map1");
+    }
+
+    public void SwapLevel2() {
+        SceneManager.LoadScene("Map2");
+    }
+
+    public void SwapLevel3() {
+        SceneManager.LoadScene("Map3");
+    }
+
+    public void SwapMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
 }
